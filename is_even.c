4b3010b121809c3b8dfc74bae6be6deb621efc6a
Wei Gao <weigao4@illinois.edu>
2020-03-07 22:49:29 -0600

    Check 8 and 9 by bitwise method

--- is_even.c
+++ is_even.c
@@ -11,12 +11,21 @@ int is_even(unsigned int i) {
     return !is_even(i-1);
 }
 
+int is_even_bitwise(unsigned int i) {
+    return ( (i>>1)<<1 == i);
+}
+
 int main() {
 
     int foo = is_even(6);
     int bar = is_even(7);
 
+    int asdf = is_even_bitwise(8);
+    int hjkl = is_even_bitwise(9);
+
     printf("6 is even: %d\n", foo);
     printf("7 is even: %d\n", bar);
+    printf("8 is even: %d\n", asdf);
+    printf("9 is even: %d\n", hjkl);
     return 0;
 }
